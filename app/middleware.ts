a322e4785c219962ea6d590e31b499d452f3cf06
Nayab Ahmad <mdnayabahmad441@gmail.com>
2026-03-08 12:53:55 +0530

    disable middleware test

--- app/middleware.ts
+++ app/middleware.ts
@@ -1,29 +1,9 @@
 import { NextResponse } from "next/server"
-import type { NextRequest } from "next/server"
 
-export function middleware(request: NextRequest) {
-
-  const host = request.headers.get("host") || ""
-  const subdomain = host.split(".")[0]
-
-  const url = request.nextUrl.clone()
-
-  // MAIN ERP DOMAIN
-  if (host.startsWith("erp.")) {
-    return NextResponse.next()
-  }
-
-  // SCHOOL SUBDOMAIN
-  if (subdomain !== "www" && subdomain !== "naysha" && subdomain !== "erp") {
-    url.pathname = `/erp${url.pathname}`
-    url.searchParams.set("school", subdomain)
-
-    return NextResponse.rewrite(url)
-  }
-
+export function middleware() {
   return NextResponse.next()
 }
 
 export const config = {
-  matcher: ["/((?!_next|api|favicon.ico).*)"],
+  matcher: ["/((?!_next|api).*)"],
 }
