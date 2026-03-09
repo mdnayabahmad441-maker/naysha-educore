db338832bf58f162c052cf95ea1b9be997cba067
Nayab Ahmad <mdnayabahmad441@gmail.com>
2026-03-09 23:44:54 +0530

    fix school subdomain redirect

--- middleware.ts
+++ middleware.ts
@@ -3,17 +3,24 @@ import { NextRequest, NextResponse } from "next/server"
 export function middleware(req: NextRequest) {
 
   const host = req.headers.get("host") || ""
-
   const url = req.nextUrl.clone()
 
   const rootDomain = "erp.naysha.online"
 
+  // MAIN WEBSITE
   if (host === rootDomain || host.includes("vercel.app")) {
     return NextResponse.next()
   }
 
+  // GET SUBDOMAIN
   const subdomain = host.replace(`.${rootDomain}`, "")
 
+  // If school homepage opened → redirect to ERP login
+  if (url.pathname === "/") {
+    return NextResponse.redirect(new URL("/erp/login", req.url))
+  }
+
+  // Pass school to app
   url.searchParams.set("school", subdomain)
 
   return NextResponse.rewrite(url)
