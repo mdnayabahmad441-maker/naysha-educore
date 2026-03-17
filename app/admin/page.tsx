038fd5dcbd1f4d0054e8771197c6a6535831d9db
Nayab Ahmad <mdnayabahmad441@gmail.com>
2026-03-17 10:23:51 +0530

    added admin page

---
+++ app/admin/page.tsx
@@ -0,0 +1,18 @@
+"use client"
+
+export default function AdminDashboard() {
+
+  return (
+    <div className="min-h-screen bg-[#020c1b] text-white p-10">
+
+      <h1 className="text-3xl font-bold mb-6">
+        Admin Dashboard
+      </h1>
+
+      <p className="text-gray-400">
+        Welcome to your school ERP dashboard.
+      </p>
+
+    </div>
+  )
+}
