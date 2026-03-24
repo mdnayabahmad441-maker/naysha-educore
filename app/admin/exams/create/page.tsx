aad9782bde09d5791097cd5c0d0862413226c55d
Nayab Ahmad <mdnayabahmad441@gmail.com>
2026-03-24 09:42:04 +0530

    fix create exam page error

---
+++ app/admin/exams/create/page.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+export default function CreateExamPage() {
+  return (
+    <div className="p-6 text-white">
+      <h1 className="text-2xl font-semibold">
+        Create Exam
+      </h1>
+    </div>
+  )
+}
